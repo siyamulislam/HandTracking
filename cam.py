932b98cf6ce417fcb10cd0c0c2d61b00b4bea4d0
siyamulislam <siyamulislam1@gmail.com>
2023-03-12 17:30:49 +0600

    basic camera option

---
+++ cam.py
@@ -0,0 +1,41 @@
+import cv2
+
+record = False
+
+cap = cv2.VideoCapture(0)
+
+if (cap.isOpened() == False): 
+  print("Unable to read camera feed")
+
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+print(frame_height,frame_width)
+
+out = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
+
+while(True):
+  ret, frame = cap.read()
+  k = cv2.waitKey(1)
+  cv2.imshow('frame',frame)
+
+    # press space key to start recording
+  if k%256 == 32:
+    if record==False:
+        record = True
+        print('recoding start') 
+    else:
+        record=False
+        print('recoding pause')
+  if record:
+    out.write(frame) 
+
+    # press q key to close the program
+  if k & 0xFF == ord('q'):
+    break
+
+ 
+
+cap.release()
+out.release()
+
+cv2.destroyAllWindows()
